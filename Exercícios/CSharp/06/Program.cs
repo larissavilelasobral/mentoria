3b69f0a9371e6578678dd0e894b1bbb1038b5511
larissavilelasobral <larissavilelasobral@gmail.com>
2022-06-17 01:37:30 -0300

    ex arrys

--- Exercícios/CSharp/06/Program.cs
+++ Exercícios/CSharp/06/Program.cs
@@ -1,7 +1,15 @@
-﻿class Program
+﻿class PassingRefByVal
 {
-  static void Main(string[] args)
-  {
-    Console.WriteLine("Hello World");
-  }
+    static void Change(int[] pArray)
+    {
+        pArray[0] = 888;  // Mudar o primeiro elemento do array
+    }
+
+    static void Main()
+    {
+        int[] arr = {1, 4, 5};
+        Console.WriteLine("primeiro numero antes: "+arr[0]);
+        Change(arr);
+        Console.WriteLine("primeiro numero depois: {0}", arr [0]);
+    }
 }
